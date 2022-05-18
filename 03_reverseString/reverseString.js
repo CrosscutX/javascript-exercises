98d8dfd64da47902920b0fd2b98c50950c650ead
Alex <crosscutcode@gmail.com>
2022-05-18 16:40:22 -0400

    Reverse the string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(newStr) {
+    let revStr = newStr.split("").reverse().join("");
+    return revStr;
 };
-
+    console.log(reverseString('fuck'));
 // Do not edit below this line
 module.exports = reverseString;
