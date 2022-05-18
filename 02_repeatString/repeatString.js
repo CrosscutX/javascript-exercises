8d7cfb66f83004b2eb08f0fc1d86c345066acc90
Alex <crosscutcode@gmail.com>
2022-05-18 15:56:32 -0400

    Pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let repStr = '';
+   if(num < 0){
+       return 'ERROR';
+   } 
+    for(let i = 1; i <= num; i++){
+        repStr += str;
+    }
+    return repStr;
 };
+
+repeatString('hey',5);
 
 // Do not edit below this line
 module.exports = repeatString;
