15cbd792ffad82bf2cc49256666e0ec76bd99a40
Alex <crosscutcode@gmail.com>
2022-05-19 01:09:38 -0400

    .includes was the key

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const testArray = ['Alex', "Wheeler", "Matt", 1,2];
 
-};
+const removeFromArray = function(arr, ...arg) {
+    const array = arr;
+    let newArray = [];
 
+    arr.forEach(item =>{
+        if(!arg.includes(item)){
+            newArray.push(item);
+        }
+    });
+    return newArray;
+}
+
+console.log(removeFromArray(testArray, 2, 1));
 // Do not edit below this line
 module.exports = removeFromArray;
