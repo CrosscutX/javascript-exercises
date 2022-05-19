d525384e7e06bca474e7773b8371475e9db0faf2
Alex <crosscutcode@gmail.com>
2022-05-19 19:53:52 -0400

    sumAll function works and form validation works

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let newSum = 0;
+    
+    if(int1 < 0 || int2 < 0 || typeof int1 === 'string' || typeof int2 === 'string' || isNaN(int1)
+            == true || isNaN(int2) == true){
+        return 'ERROR';
+    }
 
+    if(int1 > int2){
+        for(int2; int2 <= int1; int2++){
+            newSum += int2;
+        }
+    }else if(int1 < int2){
+        for(int1; int1 <= int2; int1++){
+            newSum += int1;
+        }
+    }
+    return newSum;
 };
+
+console.log(sumAll(5,"6"));
 
 // Do not edit below this line
 module.exports = sumAll;
