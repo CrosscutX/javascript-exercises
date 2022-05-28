be49340b928157cf7370dc5724e8363082951c1a
CrosscutX <103855963+CrosscutX@users.noreply.github.com>
2022-05-28 15:39:39 -0400

    Update reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,6 +2,6 @@ const reverseString = function(newStr) {
     let revStr = newStr.split("").reverse().join("");
     return revStr;
 };
-    console.log(reverseString('fuck'));
+    console.log(reverseString('hello'));
 // Do not edit below this line
 module.exports = reverseString;
