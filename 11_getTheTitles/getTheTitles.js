0ae7fb9f95e74e3c53d7ae43897e8ad5cfb821e1
Alex <crosscutcode@gmail.com>
2022-05-28 21:20:31 -0400

    Get the titles is functional

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+]
 
+const getTheTitles = function(arr) {
+    return arr.map(getTitle);
 };
+console.log(getTheTitles(books));
 
+function getTitle(book){
+    return book.title;
+}
 // Do not edit below this line
 module.exports = getTheTitles;
