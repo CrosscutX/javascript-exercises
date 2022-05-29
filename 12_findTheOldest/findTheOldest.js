ea81ad30ff30393fc211c361ef5e54ab1f2eed5e
Alex <crosscutcode@gmail.com>
2022-05-29 14:58:36 -0400

    Finish exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,45 @@
-const findTheOldest = function() {
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+  
 
+  const findTheOldest = function(array) {
+    return array.reduce((oldest, current)=>{
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+    if(oldestAge > currentAge){
+        return oldest;
+    }else if(currentAge > oldestAge){
+        return current;
+    }
+   })
 };
+
+console.log(findTheOldest(people).name)
+
+function getAge(birth, death){
+    if (!death){
+        death = new Date().getFullYear();
+    }
+        return death - birth;
+    };
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
