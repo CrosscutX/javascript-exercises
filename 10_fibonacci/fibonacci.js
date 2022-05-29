9ca40c5cf2e11f73a42045c99bf7a4d11e427fea
Alex <crosscutcode@gmail.com>
2022-05-28 20:48:42 -0400

    Finish the sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(location) {
+    let fib = [1,1];
+    for(let i = 2; i <= 25; i++){
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    if(location < 0){
+        return 'OOPS'
+    }else{
+        return fib[location - 1];
+    }
+   
 };
-
+console.log(fibonacci('6'));
 // Do not edit below this line
 module.exports = fibonacci;
